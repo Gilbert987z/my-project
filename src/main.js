f7927c98d91fa5cd3f5049289cc6ff4350530360
“zhangzhe” <12345678@qq.com>
2021-06-15 10:30:14 +0800

    Element-plus update to Element-ui

--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import router from './router'
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-
-
-
-Vue.use(ElementPlus)
+Vue.use(ElementUI)
 Vue.use(VueAxios, axios)
-// import axios from 'axios';
-// Vue.prototype.$axios = axios;   //全局注册，使用方法为:this.$axios
+Vue.prototype.$axios = axios;   //全局注册，使用方法为:this.$axios
 
 axios.defaults.baseURL = 'https://api-test.shall-buy.top'
 
@@ -27,30 +23,3 @@ new Vue({
 
 
 
-
-
-// import App from './App.vue'
-// import router from './router'
-// import { createApp } from 'vue'
-// import axios from 'axios'
-// import VueAxios from 'vue-axios'
-// import ElementPlus from 'element-plus';
-// import 'element-plus/lib/theme-chalk/index.css';
-//
-//
-//
-// const app = createApp(App)
-// app.use(ElementPlus)
-// app.use(VueAxios, axios)
-// // import axios from 'axios';
-// // Vue.prototype.$axios = axios;   //全局注册，使用方法为:this.$axios
-//
-// axios.defaults.baseURL = 'https://api-test.shall-buy.top'
-//
-// app.config.productionTip = false
-//
-//
-// new app({
-//   router,
-//   render: h => h(App),
-// }).$mount('#app')
